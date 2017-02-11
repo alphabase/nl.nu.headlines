3244af8513310a30588a0392f1d38863417b6b97
Gerard Nijboer <me@gerardnijboer.com>
2017-02-11 23:23:04 +0100

    Support smartphone speech if available.

--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 // This is an array with the known RSS XML feeds that can be found at http://www.nu.nl/rss/__category__
 var categories = ['algemeen', 'economie', 'internet', 'sport', 'achterklap', 'opmerkelijk', 'muziek', 'dvd', 'film', 'boek', 'games', 'lifehacking', 'plugged', 'auto', 'wetenschap', 'gezondheid'];
 
+var speechEngine;
+
 // Homey init function, nothing to do here
 function init() {
+	speechEngine = Homey.manager('speech-output');
 	Homey.log('NU.nl news app loaded.');
 }
 
@@ -17,34 +20,44 @@ Homey.manager('flow').on('action.nl_nu_read_news', function(callback, args) {
 // This code is run whenever a speech input was matched on this app and Homey triggers our app
 Homey.manager('speech-input').on('speech', function(speech, callback) {
 	Homey.log('Triggered by speech input.');
+	
+	// We change the speechEngine used to the variable 'speech' here, as we may have to use the phone speaker instead now
+	speechEngine = speech;
+	
 	// We check whether Homey also triggered on one of the known categories in the speech input triggers
 	var category = speech.triggers.find(function (item) {
 		return item.id == 'news_category';
 	});
+	
 	// If we were indeed triggered on a news category, we have plenty of information to continue and fetch news items
 	if (category) {
 		getNewsItems(category.text, 'title', 0);
 	} else {
 		// If we were not triggered with a news category, we have to ask the user to tell us which news category is wanted
 		Homey.log('Category unknown, asking for category input.');
-		speech.ask(__('whichCategory'), function (err, result) {
+		speechEngine.ask(__('whichCategory'), function (err, result) {
 			if (err) return Homey.error(err);
 			getNewsItems(result, 'title', 0);
 		});
 	}
+	
 	callback(null, true);
 });
 
 // This function attempts to get news items from the RSS XML feed, process them, and pass them along to the speech output manager
 function getNewsItems(category, mode, max) {
+	// We make the input category lowercase in case the Homey speech engine provides us with capitals
+	category = category.toLowerCase();
+	
 	// Check if the given category is in the array of known categories
 	var n_category = categories.find(function (item) {
-		return item == category;
+		return item == category.toLowerCase();
 	});
+	
 	// If no known category was matched, we inform the user and abort processing
 	if (!n_category) {
 		Homey.log('Unknown category: '+category);
-		Homey.manager('speech-output').say(__('unknownCategory', {'category': category}));
+		speechEngine.say(__('unknownCategory', {'category': category}));
 		return;
 	}
 	
@@ -70,18 +83,19 @@ function getNewsItems(category, mode, max) {
 			// Make sure at least one news item was found
 			if (items.length > 0) {
 				// First, we tell the total number of news items we have found
-				Homey.manager('speech-output').say(__('foundNews', {'count': items.length}));
+				speechEngine.say(__('foundNews', {'count': items.length}));
+				
 				// Then we initiate to read the first news item in the array
 				readNewsItem(items, 0, mode);
 			} else {
 				// If no news items at all have been found, we notify the user
-				Homey.manager('speech-output').say(__('noNews'));
+				speechEngine.say(__('noNews'));
 			}
 		});
 		
 		// Notify the user if any error occurs while parsing the xml rss feed
 		parser.on('error', function(error) {
-			Homey.manager('speech-output').say(__('errorParsing'));
+			speechEngine.say(__('errorParsing'));
 			console.error(error);
 		});
 		
@@ -89,7 +103,7 @@ function getNewsItems(category, mode, max) {
 		res.pipe(parser);
 	}).on('error', function(error) {
 		// If we can't download the rss feed, we notify the user
-		Homey.manager('speech-output').say(__('errorDownloading'));
+		speechEngine.say(__('errorDownloading'));
 		console.error(error);
 	});
 }
@@ -100,11 +114,12 @@ function readNewsItem(items, index, mode) {
 	if (index < items.length) {
 		// Read the title of the news item
 		Homey.log('Reading a news item: '+items[index].title);
-		Homey.manager('speech-output').say(items[index].title, function (err, success) {
+		speechEngine.say(items[index].title, function (err, success) {
+			Homey.log(success);
 			// If we also need to read the description, we continue here
 			if (mode == 'description') {
 				// Read the description of the news item
-				Homey.manager('speech-output').say(items[index].description, function (err, success) {
+				speechEngine.say(items[index].description, function (err, success) {
 					// Pause for a second and loop into the next news item
 					setTimeout(function() {
 						index ++;
