b11f879ab00aee12addd63f6c1c81a71ecab9894
Gerard Nijboer <gerard.nijboer@outlook.com>
2019-03-14 08:35:29 +0100

    Version 2.0.0
    Based on the new version of the Homey SDK, here comes a new version of the NU.nl headlines app.

--- app.js
+++ app.js
@@ -1,158 +1,161 @@
 'use strict';
 
+const Homey = require('homey');
+
 // This is an array with the known RSS XML feeds that can be found at https://www.nu.nl/rss/__category__
-var categories = ['algemeen', 'economie', 'internet', 'sport', 'achterklap', 'opmerkelijk', 'muziek', 'dvd', 'film', 'boek', 'games', 'lifehacking', 'plugged', 'auto', 'wetenschap', 'gezondheid', 'laatste'];
+let categories = ['algemeen', 'economie', 'internet', 'sport', 'achterklap', 'opmerkelijk', 'muziek', 'dvd', 'film', 'boek', 'games', 'lifehacking', 'plugged', 'auto', 'wetenschap', 'gezondheid', 'laatste'];
 
-var speechEngine;
 
-// Homey init function, nothing to do here
-function init() {
-	speechEngine = Homey.manager('speech-output');
-	Homey.log('NU.nl news app loaded.');
-	
-	// Create settings variables with default values if none seem to exist
-	if (!Homey.manager('settings').get('mode')) {
-		Homey.manager('settings').set('mode', 'title');
-		Homey.log('Created setting: mode');
-	}
-	if (!Homey.manager('settings').get('max')) {
-		Homey.manager('settings').set('max', 10);
-		Homey.log('Created setting: max');
-	}
-}
+class NUnlApp extends Homey.App {
 
-// This code is run whenever the action card is triggered in the Homey flow editor
-Homey.manager('flow').on('action.nl_nu_read_news', function(callback, args) {
-	getNewsItems(args.category, args.content, args.max);
-	callback(null, true);
-});
-
-// This code is run whenever a speech input was matched on this app and Homey triggers our app
-Homey.manager('speech-input').on('speech', function(speech, callback) {
-	Homey.log('Triggered by speech input.');
 	
-	// We change the speechEngine used to the variable 'speech' here, as we may have to use the phone speaker instead now
-	speechEngine = speech;
-	
-	// We check whether Homey also triggered on one of the known categories in the speech input triggers
-	var category = speech.triggers.find(function (item) {
-		return item.id == 'news_category';
-	});
-	
-	// If we were indeed triggered on a news category, we have plenty of information to continue and fetch news items
-	if (category) {
-		getNewsItems(category.text, Homey.manager('settings').get('mode'), Homey.manager('settings').get('max'));
-	} else {
-		// If we were not triggered with a news category, we have to ask the user to tell us which news category is wanted
-		Homey.log('Category unknown, asking for category input.');
-		speechEngine.ask(__('whichCategory'), function (err, result) {
-			if (err) return Homey.error(err);
-			if (result == false) return Homey.log('No category was provided through speech input, exiting...');
-			getNewsItems(result, Homey.manager('settings').get('mode'), Homey.manager('settings').get('max'));
-		});
-	}
-	
-	callback(null, true);
-});
-
-// This function attempts to get news items from the RSS XML feed, process them, and pass them along to the speech output manager
-function getNewsItems(category, mode, max) {
-	// We make the input category lowercase in case the Homey speech engine provides us with capitals
-	category = category.toLowerCase();
-	
-	// Check if the given category is in the array of known categories
-	var n_category = categories.find(function (item) {
-		return item == category.toLowerCase();
-	});
-	
-	// If no known category was matched, we inform the user and abort processing
-	if (!n_category) {
-		Homey.log('Unknown category: '+category);
-		speechEngine.say(__('unknownCategory', {'category': category}));
-		return;
-	}
-	
-	// The category 'laatste' should be casted to an empty string for its RSS URL to be correct
-	if (category == 'laatste') {
-		category = '';
-	}
-	
-	// Import necessary classes
-	var https = require('https');
-	var FeedMe = require('feedme');
-	
-	// Get the rss news feed from the internet
-	Homey.log('Getting news items for category: '+category);
-	https.get('https://www.nu.nl/rss/'+category, function(res) {
-		var parser = new FeedMe();
-		var items = [];
+	onInit() {
+		this.log('NU.nl news app loaded.');
+		this.initSpeech();
 		
-		// Whenever we find an item in the parsed xml, add it to the items array
-		parser.on('item', function(item) {
-			// We only add the item to the array if the max number of items has not been reached, or no max (=0) was set
-			if (items.length < max || max == 0) items.push(item);
+	}
+	
+	initSpeech() {
+		// Fix for addressing the parent object in certain contexts below
+		let object = this;
+		
+		// This events is called when your app is a possible candidate for speech input
+		Homey.ManagerSpeechInput.on('speechEval', function(speech, callback) {
+			callback(null, true);
 		});
 		
-		// When the entire feed was parsed, we continue to process the items array
-		parser.on('end', function() {
-			Homey.log('Number of news items found: '+items.length);
-			// Make sure at least one news item was found
-			if (items.length > 0) {
-				// First, we tell the total number of news items we have found
-				speechEngine.say(__('foundNews', {'count': items.length}));
-				
-				// Then we initiate to read the first news item in the array
-				readNewsItem(items, 0, mode);
+		// This code is run whenever a speech input was matched on this app and Homey triggers our app
+		Homey.ManagerSpeechInput.on('speechMatch', (speech, onSpeechEvalData) => {
+			this.log('Triggered by speech input.');
+			
+			// We check whether Homey also triggered on one of the known categories in the speech input triggers
+			if (speech.matches && speech.matches.main && speech.matches.main.categoryWords) {
+				this.getNewsItems(speech, speech.matches.main.categoryWords.value[0], 'title', 3); // TODO :IMPLEMENT SETTINGS
 			} else {
-				// If no news items at all have been found, we notify the user
-				speechEngine.say(__('noNews'));
+				// If we were triggered without a news category, we have to ask the user to tell us which news category is wanted
+				this.log('Category unknown, asking for category input.');
+				speech.ask(Homey.__('whichCategory'), {}, function(error, result) {
+					if (error) {
+						return Homey.error(error);
+					} else if (result == '') {
+						return Homey.log('No category was provided through speech input, exiting...');
+					} else {
+						object.getNewsItems(speech, result, 'title', 3); // TODO :IMPLEMENT SETTINGS
+					}
+				});
 			}
+        });
+		
+	}
+	
+	// This function attempts to get news items from the RSS XML feed, process them, and pass them along to the speech output manager
+	async getNewsItems(speechsession, category, mode, max) {
+		// We make the input category lowercase in case the Homey speech engine provides us with capitals
+		category = category.toLowerCase();
+		
+		// Check if the given category is in the array of known categories
+		var n_category = categories.find(function (item) {
+			return item == category.toLowerCase();
 		});
 		
-		// Notify the user if any error occurs while parsing the xml rss feed
-		parser.on('error', function(error) {
-			speechEngine.say(__('errorParsing'));
+		// If no known category was matched, we inform the user and abort processing
+		if (!n_category) {
+			this.log('Unknown category: '+category);
+			speechsession.say(Homey.__('unknownCategory', {'category': category}));
+			return;
+		}
+		
+		// The category 'laatste' should be casted to an empty string for its RSS URL to be correct
+		if (category == 'laatste') {
+			category = '';
+		}
+		
+		// Import necessary classes
+		var https = require('https');
+		var FeedMe = require('feedme');
+		
+		// Fix for addressing the parent object in certain contexts below
+		let object = this;
+		
+		// Get the rss news feed from the internet
+		this.log('Getting news items for category: '+category);
+		https.get('https://www.nu.nl/rss/'+category, function(res) {
+			var parser = new FeedMe();
+			var items = [];
+			
+			// Whenever we find an item in the parsed xml, add it to the items array
+			parser.on('item', function(item) {
+				// We only add the item to the array if the max number of items has not been reached, or no max (=0) was set
+				if (items.length < max || max == 0) items.push(item);
+			});
+			
+			// When the entire feed was parsed, we continue to process the items array
+			parser.on('end', function() {
+				console.log('Number of news items found: '+items.length);
+				// Make sure at least one news item was found
+				if (items.length > 0) {
+					// First, we tell the total number of news items we have found
+					speechsession.say(Homey.__('foundNews', {'count': items.length}))
+						.then(txt => {
+							// Then we initiate to read the first news item in the array
+							object.readNewsItem(speechsession, items, 0, mode);
+						});
+				} else {
+					// If no news items at all have been found, we notify the user
+					speechsession.say(Homey.__('noNews'));
+				}
+			});
+			
+			// Notify the user if any error occurs while parsing the xml rss feed
+			parser.on('error', function(error) {
+				speechsession.say(Homey.__('errorParsing'));
+				console.error(error);
+			});
+			
+			// Give the result to the parser
+			res.pipe(parser);
+		}).on('error', function(error) {
+			// If we can't download the rss feed, we notify the user
+			speechsession.say(Homey.__('errorDownloading'));
 			console.error(error);
 		});
-		
-		// Give the result to the parser
-		res.pipe(parser);
-	}).on('error', function(error) {
-		// If we can't download the rss feed, we notify the user
-		speechEngine.say(__('errorDownloading'));
-		console.error(error);
-	});
-}
-
-// This function reads one news item, pauses and loops into the next news item, if any
-function readNewsItem(items, index, mode) {
-	// Make sure there is at least one more news item to loop into
-	if (index < items.length) {
-		// Read the title of the news item
-		Homey.log('Reading a news item: '+items[index].title);
-		speechEngine.say(items[index].title, function (err, success) {
-			// If we also need to read the description, we continue here
-			if (mode == 'description') {
-				// Read the description of the news item
-				speechEngine.say(items[index].description, function (err, success) {
-					// Pause for a second and loop into the next news item
-					setTimeout(function() {
-						index ++;
-						readNewsItem(items, index, mode);
-					}, 1000);
-				});
-			} else {
-				// Pause for a second and loop into the next news item
-				setTimeout(function() {
-					index ++;
-					readNewsItem(items, index, mode);
-				}, 1000);
-			}
-		});
-	} else {
-		// Make a log entry if we have reached the end of the news items
-		Homey.log('No more news items to read.');
 	}
+
+	// This function reads one news item, pauses and loops into the next news item, if any
+	async readNewsItem(speechsession, items, index, mode) {
+		// Fix for addressing the parent object in certain contexts below
+		let object = this;
+		
+		// Make sure there is at least one more news item to loop into
+		if (index < items.length) {
+			// Read the title of the news item
+			this.log('Reading a news item: '+items[index].title);
+			speechsession.say(items[index].title)
+				.then(txt => {
+					// If we also need to read the description, we continue here
+					if (mode == 'description') {
+						// Read the description of the news item
+						speechsession.say(items[index].description, function (err, success) {
+							// Pause for a second and loop into the next news item
+							setTimeout(function() {
+								index ++;
+								object.readNewsItem(speechsession, items, index, mode);
+							}, 1000);
+						});
+					} else {
+						// Pause for a second and loop into the next news item
+						setTimeout(function() {
+							index ++;
+							object.readNewsItem(speechsession, items, index, mode);
+						}, 1000);
+					}
+				});
+		} else {
+			// Make a log entry if we have reached the end of the news items
+			this.log('No more news items to read.');
+		}
+	}
+	
 }
 
-module.exports.init = init;
+module.exports = NUnlApp;
